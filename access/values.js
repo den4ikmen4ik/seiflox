6ab69f4e84e6f36b6b3b3de37d2a7848f3d39509
den4ikmen4ik <76960511+den4ikmen4ik@users.noreply.github.com>
2025-04-15 01:20:56 +0200

    Update values.js

--- access/values.js
+++ access/values.js
@@ -4,7 +4,7 @@ var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
 var birthdate = "03.01.2007"; // Дата рождения
-var fullname = "Кліторчук Дмитро Русланович" // ФИО
+var fullname = "Будейко Дмитро Русланович" // ФИО
 var name = "Дмитро" // Имя
 
 // Паспорт
